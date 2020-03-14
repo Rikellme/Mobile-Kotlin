aac38d657966b5110a76d3c74a1a88b4ef646985
Rikellme Keven <56124549+Rikellme@users.noreply.github.com>
2020-03-14 12:38:25 -0300

    Add files via upload

---
+++ Aula2/Layout/app/src/main/java/br/com/rikellme/layout/MainActivity.kt
@@ -0,0 +1,18 @@
+package br.com.rikellme.layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+
+    }
+}
